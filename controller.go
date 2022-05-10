49beddd8d10fec101ac6d0797c867cb4c6fca443
Luke Ashe-Browne <xactly@gmail.com>
2022-05-10 12:29:34 +0100

    testing event processors

--- controller.go
+++ controller.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	informersappsv1 "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
@@ -28,8 +31,8 @@ func newController(clientset kubernetes.Interface, deploymentInformer informersa
 	}
 	deploymentInformer.Informer().AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
-			AddFunc:    handleAdd,
-			DeleteFunc: handleDel,
+			AddFunc:    c.handleAdd,
+			DeleteFunc: c.handleDel,
 		},
 	)
 	return c
@@ -45,13 +48,73 @@ func (c *controller) run(ch <-chan struct{}) {
 }
 
 func (c *controller) worker() {
+	for c.ProcessItem() {
 
+	}
 }
 
-func handleAdd(obj interface{}) {
+func (c *controller) ProcessItem() bool {
+	item, shutdown := c.queue.Get()
+	if shutdown {
+		return false
+	}
+	key, err := cache.MetaNamespaceKeyFunc(item)
+	if err != nil {
+		fmt.Print("Error getting key from cache %s\n", err.Error())
+	}
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		fmt.Printf("Error splitting key into namespace and name $s\n", err.Error())
+	}
+	c.syncDeployment(ns, name)
+
+	if err != nil {
+		//potentiually retry here (re-queue)
+		fmt.Printf("Error syncing deployment %s\n", err.Error())
+		return false
+	}
+
+	return true
+}
+
+func (c *controller) syncDeployment(ns, name string) error {
+	//create service
+	ctx := context.Background()
+	dep, err := c.deploymentLister.Deployments(ns).Get(name)
+	if err != nil {
+		fmt.Printf("Error geteting deployment from lister %s\n", err.Error())
+	}
+	svc := corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      dep.Name,
+			Namespace: ns,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Name: "http",
+					Port: 80,
+				},
+			},
+		},
+	}
+	_, err = c.clientset.CoreV1().Services(ns).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("Error create service %s\n", err.Error())
+		return err
+	}
+	fmt.Printf("Created service %s\n", svc.Name)
+	//create ingress
+
+	return nil
+}
+
+func (c *controller) handleAdd(obj interface{}) {
 	fmt.Println("add was called")
+	c.queue.Add(obj)
 }
 
-func handleDel(obj interface{}) {
+func (c *controller) handleDel(obj interface{}) {
 	fmt.Println("del was called")
+	c.queue.Add(obj)
 }
